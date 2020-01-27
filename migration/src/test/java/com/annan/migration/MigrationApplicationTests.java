d5aeffdb7a1685763b48b145355130a187716c55
Annan Cheng <annan.cheng@ul.com>
2020-01-27 13:19:22 +0100

    Add migration module

---
+++ migration/src/test/java/com/annan/migration/MigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annan.migration;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MigrationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
