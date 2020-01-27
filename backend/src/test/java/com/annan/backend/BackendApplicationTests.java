3f8a9e9fbca2e39d8a211790541911b493d024ae
Annan Cheng <annan.cheng@ul.com>
2020-01-27 12:01:13 +0100

    add docker compose

--- backend/src/test/java/com/annan/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/annan/backend/BackendApplicationTests.java
@@ -6,10 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-class BackendApplicationTests {
+public class BackendApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
